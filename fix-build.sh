19d1a0aed26bf5fb1a845480c314d072e240f9d6
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-07-28 05:33:28 +0000

    fixed build script.
    git-svn-id: http://projects.arcs.org.au/svn/grisu/branches/grisu-clients@1565 8882a08a-ce34-0410-b810-d8c7c368afb5

--- fix-build.sh
+++ fix-build.sh
@@ -5,12 +5,12 @@ cd target
 # fix grid-swing-client
 mkdir grisu-client-swing
 cd grisu-client-swing
-unzip -o ../grisu-0.3-SNAPSHOT-grisu-client-swing.jar
+unzip -o ../grisu-clients-0.3-SNAPSHOT-grisu-client-swing.jar
 #rm ../grisu-0.3-SNAPSHOT-grisu-client-swing.jar
 rm -f META-INF/INDEX.LIST
-cp ../../grisu-client-swing/src/main/resources/log4j.properties .
+cp ../../grisu-swing/src/main/resources/log4j.properties .
 cp ../../gluev12r2-ext-class-map.properties .
-jar cmf ../../grisu-client-swing/MANIFEST.MF ../grisu-dummy.jar .
+jar cmf ../../grisu-swing/MANIFEST.MF ../grisu-dummy.jar .
 cd ..
 
 cd ..
