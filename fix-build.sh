e15403d32eebc6d2cb04124c5f6d4fe15ab88f18
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-08-03 10:35:34 +1000

    Changed deploy config.

--- fix-build.sh
+++ fix-build.sh
@@ -10,7 +10,7 @@ unzip -o ../grisu-clients-0.3-SNAPSHOT-grisu-client-swing.jar
 rm -f META-INF/INDEX.LIST
 cp ../../grisu-swing/src/main/resources/log4j.properties .
 cp ../../gluev12r2-ext-class-map.properties .
-jar cmf ../../grisu-swing/MANIFEST.MF ../grisu-dummy.jar .
+jar cmf ../../grisu-swing/MANIFEST.MF ../grisu.jar .
 cd ..
 
 cd ..
