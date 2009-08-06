67ea1c620b9ae0789213b0d6c8ed3800b01b5c7b
Markus Binsteiner <markus@avus.(none)>
2009-08-06 19:06:10 +1000

    Finished first version of matchmaker integration into the swing client

--- grisu-swing/src/main/java/org/vpac/grisu/client/view/swing/template/panels/CandidateHost.java
+++ grisu-swing/src/main/java/org/vpac/grisu/client/view/swing/template/panels/CandidateHost.java
@@ -30,13 +30,15 @@ import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.RowSpec;
 import javax.swing.border.TitledBorder;
+import java.awt.BorderLayout;
+import org.vpac.grisu.client.view.swing.template.panels.helperPanels.GridResourceInfoPanel;
 
 public class CandidateHost extends JPanel implements TemplateNodePanel,
 		ValueListener, FqanListener {
-	
-	
+
 	public CandidateHost() {
-		setBorder(new TitledBorder(null, "Submission location", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		setBorder(new TitledBorder(null, "Submission location",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		setLayout(new FormLayout(new ColumnSpec[] {
 				FormFactory.RELATED_GAP_COLSPEC,
 				ColumnSpec.decode("200dlu"),
@@ -53,19 +55,19 @@ public class CandidateHost extends JPanel implements TemplateNodePanel,
 				RowSpec.decode("default:grow"),
 				FormFactory.RELATED_GAP_ROWSPEC,}));
 		add(getLblSite(), "2, 1");
-		add(getInfoPanel(), "3, 1, 1, 7, fill, fill");
+		add(getGridResourceInfoPanel(), "4, 1, 1, 7, fill, fill");
 		add(getSiteComboBox(), "2, 3, fill, default");
 		add(getLblQueue(), "2, 5");
 		add(getQueueComboBox(), "2, 7, fill, top");
 	}
-	
+
 	static final Logger myLogger = Logger.getLogger(CandidateHost.class
 			.getName());
-	
+
 	private String currentSubmissionLocation;
 	private String currentVersion;
 	private String currentFqan;
-	
+
 	private String applicationName;
 
 	private GrisuRegistry registry;
@@ -75,32 +77,35 @@ public class CandidateHost extends JPanel implements TemplateNodePanel,
 	private JLabel lblQueue;
 	private JComboBox queueComboBox;
 	private JComboBox siteComboBox;
-	private JPanel infoPanel;
-	
-	private SortedSet<RankedSite> currentRankedSites; 
+
+	private SortedSet<RankedSite> currentRankedSites;
 	private RankedSite currentRankedSite;
-	
+
+	private boolean lockQueueCombo = false;
+
 	private DefaultComboBoxModel siteModel = new DefaultComboBoxModel();
 	private DefaultComboBoxModel queueModel = new DefaultComboBoxModel();
-	
+	private GridResourceInfoPanel gridResourceInfoPanel;
+
 	public JPanel getTemplateNodePanel() {
 		return this;
 	}
-
 
 	public void setTemplateNode(TemplateNode node)
 			throws TemplateNodePanelException {
 
 		this.templateNode = node;
 		this.templateNode.setTemplateNodeValueSetter(this);
-		
-		
-		this.applicationName = this.templateNode.getTemplate().getApplicationName();
-		
-		serviceInterface = node.getTemplate().getEnvironmentManager().getServiceInterface();
+
+		this.applicationName = this.templateNode.getTemplate()
+				.getApplicationName();
+
+		serviceInterface = node.getTemplate().getEnvironmentManager()
+				.getServiceInterface();
 		registry = GrisuRegistry.getDefault(serviceInterface);
 
-		this.currentFqan = registry.getUserEnvironmentManager().getCurrentFqan(); 
+		this.currentFqan = registry.getUserEnvironmentManager()
+				.getCurrentFqan();
 	}
 
 	public String getExternalSetValue() {
@@ -109,51 +114,62 @@ public class CandidateHost extends JPanel implements TemplateNodePanel,
 
 	public void reset() {
 	}
+
 	public void templateNodeUpdated(TemplateNodeEvent event) {
 	}
-
 
 	public void setExternalSetValue(String value) {
 		myLogger.warn("Not supported yet.");
 		throw new RuntimeException("Setting value not supported yet.");
 	}
-	
+
 	private void calculateAvailableGridResources() {
-		
+
 		siteModel.removeAllElements();
-		
+
 		Map<JobSubmissionProperty, String> jobProperties = new HashMap<JobSubmissionProperty, String>();
-		jobProperties.put(JobSubmissionProperty.APPLICATIONNAME, applicationName);
-		jobProperties.put(JobSubmissionProperty.APPLICATIONVERSION, currentVersion);
-		
-		SortedSet<GridResource> resources = registry.getUserApplicationInformation(applicationName).getBestSubmissionLocations(jobProperties, currentFqan);
-		
+		jobProperties.put(JobSubmissionProperty.APPLICATIONNAME,
+				applicationName);
+		jobProperties.put(JobSubmissionProperty.APPLICATIONVERSION,
+				currentVersion);
+
+		SortedSet<GridResource> resources = registry
+				.getUserApplicationInformation(applicationName)
+				.getBestSubmissionLocations(jobProperties, currentFqan);
+
 		currentRankedSites = GridResourceHelpers.asSetOfRankedSites(resources);
-		
-		for ( RankedSite site : currentRankedSites ) {
+
+		for (RankedSite site : currentRankedSites) {
 			siteModel.addElement(site);
 		}
-		
+
 	}
-	
+
 	private void fillQueueCombobox() {
-		
-		if ( siteModel.getSize() > 0 ) {
-		
-		currentRankedSite = (RankedSite)siteComboBox.getSelectedItem();
-		
-		queueModel.removeAllElements();
-		
-		for ( GridResource resource : currentRankedSite.getResources() ) {
-			queueModel.addElement(resource);
+
+		if (siteModel.getSize() > 0) {
+
+			currentRankedSite = (RankedSite) siteComboBox.getSelectedItem();
+
+			if (currentRankedSite == null) {
+				return;
+			}
+
+			queueModel.removeAllElements();
+
+			for (GridResource resource : currentRankedSite.getResources()) {
+				queueModel.addElement(resource);
+			}
+		} else {
+			currentRankedSite = null;
+			currentSubmissionLocation = null;
 		}
-		}
-		
+
 	}
 
 	public void valueChanged(TemplateNodePanel panel, String newValue) {
-		
-		if ( panel instanceof ApplicationVersion ) {
+
+		if (panel instanceof ApplicationVersion) {
 			this.currentVersion = newValue;
 			calculateAvailableGridResources();
 		}
@@ -167,10 +183,9 @@ public class CandidateHost extends JPanel implements TemplateNodePanel,
 
 	public void removeValueListener(ValueListener v) {
 	}
+
 	public void addValueListener(ValueListener v) {
 	}
-
-
 
 	private JLabel getLblSite() {
 		if (lblSite == null) {
@@ -178,52 +193,76 @@ public class CandidateHost extends JPanel implements TemplateNodePanel,
 		}
 		return lblSite;
 	}
+
 	private JLabel getLblQueue() {
 		if (lblQueue == null) {
 			lblQueue = new JLabel("Queue:");
 		}
 		return lblQueue;
 	}
+
 	private JComboBox getQueueComboBox() {
 		if (queueComboBox == null) {
 			queueComboBox = new JComboBox(queueModel);
 			queueComboBox.addItemListener(new ItemListener() {
 				public void itemStateChanged(ItemEvent arg0) {
-					
-					if ( queueModel.getSize() > 0 ) {
 
 					try {
-						currentSubmissionLocation = SubmissionLocationHelpers.createSubmissionLocationString(((GridResource)queueModel.getSelectedItem()));
+
+						if (!lockQueueCombo) {
+							if (queueModel.getSize() > 0) {
+								Object o = queueModel.getSelectedItem();
+								if (o == null) {
+									return;
+								}
+								try {
+									setCurrentSubmissionLocation((GridResource)o);
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+
+						}
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
-					}
-					
-					
 				}
 			});
 		}
 		return queueComboBox;
 	}
+	
+	private void setCurrentSubmissionLocation(GridResource r) {
+		currentSubmissionLocation = SubmissionLocationHelpers.createSubmissionLocationString(r);
+		getGridResourceInfoPanel().setGridResource(r);
+	}
+
 	private JComboBox getSiteComboBox() {
 		if (siteComboBox == null) {
 			siteComboBox = new JComboBox(siteModel);
 			siteComboBox.addItemListener(new ItemListener() {
 				public void itemStateChanged(ItemEvent arg0) {
-					
-					fillQueueCombobox();
-					
+					try {
+						lockQueueCombo = true;
+						fillQueueCombobox();
+						lockQueueCombo = false;
+						if (currentRankedSite != null) {
+							Object o = queueModel.getSelectedItem();
+							setCurrentSubmissionLocation((GridResource)o);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
 			});
 		}
 		return siteComboBox;
 	}
-	private JPanel getInfoPanel() {
-		if (infoPanel == null) {
-			infoPanel = new JPanel();
-			infoPanel.setLayout(new FormLayout(new ColumnSpec[] {},
-				new RowSpec[] {}));
+
+	private GridResourceInfoPanel getGridResourceInfoPanel() {
+		if (gridResourceInfoPanel == null) {
+			gridResourceInfoPanel = new GridResourceInfoPanel();
 		}
-		return infoPanel;
+		return gridResourceInfoPanel;
 	}
 }
